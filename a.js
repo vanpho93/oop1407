39fa9f357b83aae16e7ecf82f5480f5e8b1a4fa6
Van Pho Nguyen <vanpho02@gmail.com>
2017-07-21 19:43:45 +0700

    xong

---
+++ a.js
@@ -0,0 +1,15 @@
+// const arrN = new Array();
+// const arrNum = [1, 'javascript', console.log];
+
+const arrSubject = [
+    'NodeJS', 'ReactJS', 'React Native', 'PHP', 'Java', 'Android'
+];
+
+// arrSubject.push('iOS');
+// arrSubject.unshift('Python');
+// arrSubject.pop();
+// arrSubject.shift();
+// arrSubject.splice(1, 2);
+// arrSubject.splice(2, 0, 'iOS', 'Unity');
+
+console.log(arrSubject);
